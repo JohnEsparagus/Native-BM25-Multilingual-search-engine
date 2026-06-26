f98ee00e1fc61f4597f1d07b8d794007e1e15bf6
JohnEsparagus <johnedesparagused@gmail.com>
2026-06-26 21:45:20 +0800

    Created document classes and a simple corpus to test a small input

---
+++ src/bm25.cpp
@@ -0,0 +1,7 @@
+#include "headers/bm25.hpp"
+
+std::vector<std::string> corpus = {
+    "The quick brown fox jumps over the lazy dog",
+    "A brown fox is fast and furious, but the lazy dog is sleeping in the grass today",
+    "Foxes are very fast animals. The quick fox loves jumping over lazy dogs in the yard",
+    "The quick brown fox"};
---
+++ src/headers/document.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <unordered_map>
+#include <string>
+
+class Document{
+private:
+    
+public:
+    std::unordered_map<std::string, size_t> term_freq; 
+    Document(const std::string& document);
+
+    size_t len;
+};
+
+
+class Engine{
+public:
+    size_t avg_doc_len;
+    size_t total_docs;
+    size_t doc_frequency;
+
+};
